3bce37c9a9106293e2b18adfd83db16e0a013730
Burenzaya <bvrnee1009@gmail.com>
2023-11-12 19:43:28 +0800

    үргэлж шилжүүлэг хийж байхаар

--- temp_con.php
+++ temp_con.php
@@ -1,28 +1,30 @@
 <html>
 
 <head>
-  <title>Temperature Conversion</title>
+    <title>Temperature Conversion</title>
 </head>
 
 <body>
-  <?php
-if (isset ( $_GET ['fahrenheit'] )) {
-$fahrenheit = $_GET ['fahrenheit'];
-} else {
-$fahrenheit = null;
-}
-if (is_null ( $fahrenheit )) {
-  ?>
-  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
-    Fahrenheit temperature: <input type="text" name="fahrenheit" /><br />
-    <input type="submit" value="Convert to Celsius!" />
-  </form>
-  <?php
-  } else {
-  $celsius = ($fahrenheit - 32) * 5 / 9;
-  printf ( "%.2fF is %.2fC", $fahrenheit, $celsius );
-  }
-  ?>
+    <?php if ($_SERVER['REQUEST_METHOD'] == 'GET') { ?>
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+            Fahrenheit temperature:
+            <input type="text" name="fahrenheit" /><br />
+            <input type="submit" value="Convert to Celsius!" />
+        </form>
+    <?php } else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $fahrenheit = $_POST['fahrenheit'];
+        $celsius = ($fahrenheit - 32) * 5 / 9;
+        printf("%.2fF is %.2fC", $fahrenheit, $celsius); ?>
+
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+            Fahrenheit temperature:
+            <input type="text" name="fahrenheit" /><br />
+            <input type="submit" value="Convert to Celsius!" />
+        </form>
+
+    <?php } else {
+        die("This script only works with GET and POST requests.");
+    } ?>
 </body>
 
 </html>
