3e7333d2f2d336ecdbbecb9c0a3962e71bb01e99
Burenzaya <bvrnee1009@gmail.com>
2023-11-12 23:22:03 +0800

    celsius <-> faranhet

--- temp_con.php
+++ temp_con.php
@@ -1,26 +1,48 @@
 <html>
 
 <head>
-    <title>Temperature Conversion</title>
+    <meta charset="UTF-8">
+    <title>Температур хөрвүүлэгч</title>
 </head>
 
 <body>
     <?php if ($_SERVER['REQUEST_METHOD'] == 'GET') { ?>
-        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
-            Fahrenheit temperature:
-            <input type="text" name="fahrenheit" /><br />
-            <input type="submit" value="Convert to Celsius!" />
-        </form>
-    <?php } else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $fahrenheit = $_POST['fahrenheit'];
-        $celsius = ($fahrenheit - 32) * 5 / 9;
-        printf("%.2fF is %.2fC", $fahrenheit, $celsius); ?>
 
         <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
-            Fahrenheit temperature:
-            <input type="text" name="fahrenheit" /><br />
-            <input type="submit" value="Convert to Celsius!" />
+            Фаренгейтын температур:
+            <input type="text" name="F" /><br />
+            <input type="submit" value="Цельс-рүү хөрвүүл!" />
         </form>
+        
+    <?php } else if ($_SERVER['REQUEST_METHOD'] == 'POST') { ?>
+
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+            Фаренгейтын температур:
+            <input type="hidden" name="indicator" value="1" />
+            <input type="text" name="F" /><br />
+            <input type="submit" value="Цельс-рүү хөрвүүл!" />
+        </form>
+
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+            Цельсын температур:
+            <input type="hidden" name="indicator" value="0" />
+            <input type="text" name="C" /><br />
+            <input type="submit" value="Фарангейт-рүү хөрвүүл!" />
+        </form>
+
+        <?php
+        if($_POST['indicator']){
+            $F = $_POST['F'];
+            $C = ($F - 32) * 5 / 9;
+            printf("%.2fF is %.2fC", $F, $C);
+        }
+        else{
+            $C = $_POST['C'];
+            $F = ($C * (9 / 5)) + 32;
+            printf("%.2fC is %.2fF", $C, $F);
+        }
+        ?>
+        
 
     <?php } else {
         die("This script only works with GET and POST requests.");
